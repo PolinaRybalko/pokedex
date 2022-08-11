6165f92bd150b5cdf4eedfe3c69bc6b215e215e2
Polina Rybalko <polinarybalko@gmail.com>
2022-08-12 00:13:56 +0300

    Add types to Pokemon Card

--- src/components/List/PokemonCard.js
+++ src/components/List/PokemonCard.js
@@ -1,11 +1,20 @@
 import "./PokemonCard.css";
 
 function PokemonCard(props) {
+    function display(string)
+    {
+        return (string[0].toUpperCase() + string.slice(1));
+    }
   const thisPokemon = props.pokemon;
   return (
     <div className="PokemonCard">
       <img alt={`${thisPokemon.name} depiction`} src={thisPokemon.imgURL} />
-      {thisPokemon.name}
+      {display(thisPokemon.name)}
+      <div className="PokemonCard__types">
+        {thisPokemon.types.map((type) => (
+          <button key={thisPokemon.name + type}>{display(type)}</button>
+        ))}
+      </div>
     </div>
   );
 }
